133e27f61ff8ce277f0b1dd7c510cf8dca571a95
kleme <klemens.raduner@nyp.ch>
2020-03-12 10:39:00 +0100

    deleted files

--- Unbenannt2.ps1
+++
@@ -1,7 +0,0 @@
-﻿
-$Form                            = New-Object system.Windows.Forms.Form
-$Form.ClientSize                 = '400,230'
-$Form.text                       = "Test"
-$Form.TopMost                    = $false
-$Icon                            = New-Object system.drawing.icon ('IMG_0360.ico')
-$Form.Icon                       = $Icon
